c78834350d55949de644c6a70fc31f96862f0287
hvvvy <63624740+hvvvy@users.noreply.github.com>
2020-11-05 12:33:11 +0900

    random.hのメンバ関数をインライン関数に変更

--- src/random.h
+++ src/random.h
@@ -1,11 +1,25 @@
 #ifndef RANDOM_H_
 #define RANDOM_H_
 
+#include "random.h"
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+
 class CRandom{
 
 public:
-	void InitRand();
-	 int random(int size);
+	//インライン関数
+	inline void InitRand()
+	{
+		//システム内の時間を基に初期化
+		srand((unsigned int)time(NULL));
+	}
+	//インライン関数
+	 inline int random(int size)
+	 {
+	 	return rand() % size;
+	 }
 };
 
 
